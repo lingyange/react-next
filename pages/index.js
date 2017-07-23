f941aff7f62f431ae619506b40d958e75d05af4c
jx.hu <hjx8901234@163.com>
2017-07-23 22:43:10 +0800

    更新学习笔记，前后端渲染问题

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,53 @@
-import Link from 'next/link';
-import MyLayout from '../components/MyLayout';
+// import Link from 'next/link';
+// import MyLayout from '../components/MyLayout';
 
-const PostLink = (props) => (
-    <li>
-        <Link as ={`/p/${props.id}`} href={`/post?title=${props.id}`}>
-            <a>{props.title}</a>
-        </Link>
-    </li>
-)
+// const PostLink = (props) => (
+//     <li>
+//         <Link as ={`/p/${props.id}`} href={`/post?title=${props.id}`}>
+//             <a>{props.title}</a>
+//         </Link>
+//     </li>
+// )
 
-export default() => (
-    <MyLayout>
-        <h1>My Blog</h1>
+// export default() => (
+//     <MyLayout>
+//         <h1>My Blog</h1>
+//         <ul>
+//             <PostLink id="hello-nextjs" title="Hello Next.js"/>
+//             <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
+//             <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
+//         </ul>
+//     </MyLayout>
+// )
+
+import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+
+const Index = (props) => (
+    <Layout>
+        <h1>Batman TV show </h1>
         <ul>
-            <PostLink id="hello-nextjs" title="Hello Next.js"/>
-            <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
-            <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
+            {props.shows.map(({show})=>(
+                <li key={show.id}>
+                    <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`} >
+                        <a href={show.url}>{show.name}</a>
+                    </Link>
+                </li>
+            ))}
         </ul>
-    </MyLayout>
+    </Layout>
 )
+
+Index.getInitialProps = async function(){
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await res.json()
+
+    console.log('Show data fetched. Count :'+`${data.length}`);
+
+    return {
+        shows:data
+    }
+}
+
+export default Index
--- pages/post.js
+++ pages/post.js
@@ -1,14 +1,28 @@
-import Layout from "../components/MyLayout.js"
+// import Layout from "../components/MyLayout.js" const Content = (props)=>(
+// <div>     <h1>{props.url.query.title}</h1>     <p>This is the blog post
+// content.{props.url.query.title}</p>     </div> ) export default (props) => (
+//    <Layout>         <Content url = {props.url} />     </Layout> )
+import Layout from '../components/MyLayout'
+import fetch from 'isomorphic-unfetch'
 
-const Content = (props)=>(
-    <div>
-    <h1>{props.url.query.title}</h1>
-    <p>This is the blog post content.{props.url.query.title}</p>
-    </div>
-)
-
-export default (props) => (
+const Post = (props) => (
     <Layout>
-        <Content url = {props.url} />
+        <h1>{props.show.name}</h1>
+        <p>{props
+                .show
+                .summary
+                .replace(/<[/]?p>/g, '')}</p>
+        <img src={props.show.image.medium}/>
     </Layout>
 )
+
+Post.getInitialProps = async function (context) {
+    const {id} = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+    const show = await res.json();
+
+    console.log(`Fetchde show :${show.name}`);
+    return {show}
+}
+
+export default Post
